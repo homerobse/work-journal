40bcc5f341cfb29a63498b46176e3c043bff68f4
Homero <homero.esmeraldo@gmail.com>
2025-01-06 21:15:38 -0800

    Add new work tags

--- view.py
+++ view.py
@@ -32,7 +32,7 @@ RESEARCH_TAGS = ['ucsd_mattarlab_seqs', 'ucsd_mattarlab_proj','ucsd_proj', 'ucsd
         'ucsd_mattarlab_mouse-maze', 'ucsd_mattarlab_maze', 'ucsd_mouse-maze',
         'ucsd_mattarlab_ti', 'oxford_ti', 'oxford_ti-draft', 'oxford_proj', 'oxford_paper',
         'ucsd_neuromllab_metrics',
-        'funding', 'dissertation',  'ucsd_advancement', 'ucsd_research',
+        'funding', 'dissertation',  'ucsd_advancement', 'ucsd_research', 'thesis',
         'phd-applications', 'applications', 'ucsd_mattarlab_peer-review']
 READING_GROUP_TAGS = ["ucsd_dayanabbott-rg", "ucsd_planning-rg", "planning-rg", "ucsd_book-club", "ucsd_yu-jc",
                       "ucsd_neurotheory-jc", "jotun-rg", "jotun-jc", "ucsd_tem-rg", "bishop-rg", "ucsd_hpc-rg",
@@ -40,16 +40,19 @@ READING_GROUP_TAGS = ["ucsd_dayanabbott-rg", "ucsd_planning-rg", "planning-rg", 
                       "ucsd_jc", "bookclub", "ucsd_jerniganlab_journal-club"]
 # TODO: should I add "reading"? check what are the reading TAGS that exist and if they are about reading group or general study. 
 # Also, will it be a problem that reading_neuronal-dynamics is a subtag of reading?
-STUDY_TAGS = READING_GROUP_TAGS + ['ucsd_class', 'ucsd_course', 'ucsd_talk', 'literature_scan', 
-                'literature_reading', 'talk', 'ufrn_talk', 'ucsd_reading', 
+STUDY_TAGS = READING_GROUP_TAGS + ['ucsd_class', 'ucsd_course', 'literature_scan',
+                'literature_reading', 'literature_meeting',
+                'talk', 'ufrn_talk', 'ucsd_talk',
+                'ucsd_reading',
                 'sideways-investigation', 'ucsd_sideways-investigation', 'sideways-investigation',
                 'ucsd_mattarlab_lab-meeting', 'ucsd_jerniganlab_lab-meeting', 'ucsd_lab-meeting',
+                'ucsd_neuromllab_lab-meeting',
                 'ucsd_bazhenovlab_lab-meeting', 'ucsd_mattarlab_meeting', 'ucsd_mattarlab_luke', 
                 'ucsd_sejnowskilab_lab-meeting', 'conference_sfn', 'ucsd_writinghub',
                 'ucsd_sejnowskilab_meeting', 'ucsd_jerniganlab_meeting']
 # 'ucsd_mattarlab': not sure how to cover a tag that is a parent tag for another one (ucsd_mattarlab_seqs) that I already count elsewhere]
 ADMIN_TAGS = ["ucsd_admin", "ucsd_email", "oxford_paperwork", "oxford_email", "admin", 
-                "ucsd_paperwork", "organization", "ucsd_visa"]
+                "ucsd_paperwork", "organization", "ucsd_visa", "msg"]
 TAGS = RESEARCH_TAGS + STUDY_TAGS + ADMIN_TAGS + ["ucsd_ta", "ucsd_tech",
           "rest", "personal", "procrastination", "maiseducacao", "trustedcrowd"]  # non-work related
 # Anything not included in the TAGS list will be listed together as "Other". Check code that separate worked time into categories
@@ -355,7 +358,7 @@ def get_month_range(year, month):
 
 
 def is_day_off(date):
-    """ Check if given date is day off (i.e. weekend, holiday or vacation).
+    """Check if given date is day off (i.e. weekend, holiday or vacation).
     :param (datetime.date) date
     :return (bool) True if it is a date of a day-off, False if not.
     """
